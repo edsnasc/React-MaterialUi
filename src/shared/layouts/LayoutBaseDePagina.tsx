9bcd8e46082f84cbc17a7a4f5821cc46df7d8d4e
Edson Nascimento <edsnascimento89@gmail.com>
2024-06-27 14:16:04 -0300

    [new]: creating the responsive menu header

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -5,34 +5,43 @@ import { DrawerContext } from '../contexts';
 
 interface ILayoutBaseDePaginaProps {
   titulo: string;
+  barraDeFerramentas?: ReactNode;
   children: ReactNode;
 }
 
-export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo }) => {
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo, barraDeFerramentas }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
 
   const { toggleDrawerOpen } = useContext(DrawerContext)
 
   return (
     <Box height='100%' display='flex' flexDirection='column' gap={1}>
-      <Box padding={1} display='flex' alignItems='center' height={theme.spacing(12)} gap={1}>
+      <Box padding={1} display='flex' alignItems='center' height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} gap={1}>
         {smDown && (
           <IconButton onClick={toggleDrawerOpen}>
             <Icon>menu</Icon>
           </IconButton>
         )}
 
-        <Typography variant='h5'>
+        <Typography
+          overflow='hidden'
+          whiteSpace='nowrap'
+          textOverflow='ellipsis'
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+        >
           {titulo}
         </Typography>
       </Box>
 
-      <Box>
-        Barra de ferramentas
-      </Box>
+      {barraDeFerramentas && (
+        <Box>
+          {barraDeFerramentas}
+        </Box>
+      )}
 
-      <Box>
+      <Box flex={1} overflow='auto'>
         {children}
       </Box>
 
